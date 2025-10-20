77df26224c055df52b7bb2e440127aea9317bc7d
Sharmila <sharmid2421@gmail.com>
2025-10-20 17:46:30 +0530

    java

---
+++ src/com/tnsif/oopsconcept/Animaldemo.java
@@ -0,0 +1,13 @@
+class Animaldemo{
+	
+	void sound() {
+		System.out.println("Animal can make a sound");
+	}
+}
+
+class Cat extends Animaldemo{
+	
+	void sound() {
+		System.out.println("Cat makes a sound Meowwww!!!");
+	}
+}
---
+++ src/com/tnsif/oopsconcept/Mathop.java
@@ -0,0 +1,13 @@
+package com.tnsif.oopsconcept;
+
+class MathOp{
+	
+	//Method Overloading
+	int add(int a, int b) {
+		return a + b;
+	}
+	
+	double add(double a, double b) {
+		return a + b;
+	}
+}
